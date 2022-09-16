e0d6bc9450112b3205d1b63e2acea35da33b5b0b
andres galindo <andreswe@hotmail.com>
2022-09-16 01:14:43 -0500

    se crea el modal de la seccion de galeria y descargas de la pagina

--- js.js
+++ js.js
@@ -1,11 +1,27 @@
 const menu = document.querySelector("#menu");
 const menuMobile = document.querySelector(".menu-mobile");
+const modal = document.querySelector(".modal");
+const imagenes = document.querySelectorAll(".galery");
+const img = document.querySelector(".modal--img");
+const btnCerrar = document.querySelector(".cerrar")
 
 menu.addEventListener("click", a);
 
 function a(){
     menuMobile.classList.toggle("inactive");
 }
+
+for (i = 0; i < imagenes.length; i++){
+    imagenes[i].addEventListener("click", function(e){
+        modal.classList.toggle("activo");
+        let src = e.target.src;
+        img.setAttribute("src", src);
+    })
+}
+
+btnCerrar.addEventListener("click", function(){
+    modal.classList.toggle("activo");
+})
 
 
 
