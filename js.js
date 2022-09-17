743d07117f13533d101178a8377307f6e41224c6
andres galindo <andreswe@hotmail.com>
2022-09-17 01:04:43 -0500

    se crea el boton de descargas del modal y se actualiza el menu de navegacion

--- js.js
+++ js.js
@@ -3,21 +3,27 @@ const menuMobile = document.querySelector(".menu-mobile");
 const modal = document.querySelector(".modal");
 const imagenes = document.querySelectorAll(".galery");
 const img = document.querySelector(".modal--img");
-const btnCerrar = document.querySelector(".cerrar")
+const btnCerrar = document.querySelector(".cerrar");
+const imgDescarga = document.querySelector(".enlace-img");
 
 menu.addEventListener("click", function(){
     menuMobile.classList.toggle("inactive");
 });
+
+menuMobile.addEventListener("click", function(){
+    menuMobile.classList.toggle("inactive");
+})
 
 for (i = 0; i < imagenes.length; i++){
     imagenes[i].addEventListener("click", function(e){
         modal.classList.toggle("activo");
         let src = e.target.src;
         img.setAttribute("src", src);
+        imgDescarga.setAttribute("href", src);
     })
 }
 
-btnCerrar.addEventListener("click", function(){
+modal.addEventListener("click", function(){
     modal.classList.toggle("activo");
 });
 
